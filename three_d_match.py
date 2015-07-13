17779a1e68fe654e75d6e43cc4d829bd8347eba6
rhsimplex <hendersonry@gmail.com>
2015-07-13 23:25:50 +0200

    threedsearch work

--- three_d_match.py
+++ three_d_match.py
@@ -5,14 +5,22 @@ import elasticsearch
 from os import execvp
 
 class ThreeDSearch():
-    def __init__(self):
+    def __init__(self, _stl_directory_name):
         self.output_directory = None
+        self.stl_directory_name = _stl_directory_name
 
-    def run(self, stl_directory_name):
+    def generate_images(self):
         self.output_directory = tempfile.mkdtemp()
 
         args = ['-b', '-p', 'image_match_generator.py', '--',
-                '-d', stl_directory_name,
+                '-d', self.stl_directory_name,
                 '-o', self.output_directory]
 
         execvp('blender', args)
+
+    def search_images(self):
+        pass
+
+    def run(self):
+        self.generate_images()
+        self.search_images()
