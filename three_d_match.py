cab6e7d5a23a19ddb2eba978dfdd218cc8b536ca
ryan <hendersonry@gmail.com>
2015-07-16 15:27:52 -0700

    refactor

--- three_d_match.py
+++ three_d_match.py
@@ -3,33 +3,34 @@ __author__ = 'ryan'
 import tempfile
 import elasticsearch
 from image_match.signature_database import SignatureES
-from os import spawnvp
-from os import P_WAIT
-from os.path import expanduser, abspath
+from os import spawnvp, P_WAIT, listdir
+from os.path import expanduser, abspath, join, splitext
 
-INDEX_NAME = '***REMOVED***_tester'
 
 class ThreeDSearch():
-    def __init__(self, _stl_directory_name):
-        self.output_directory = None
-        self.stl_directory_name = _stl_directory_name
+    def __init__(self, es_nodes=['localhost'], index_name='***REMOVED***_tester'):
+        self.es = elasticsearch.Elasticsearch(es_nodes)
+        self.ses = SignatureES(self.es, index=index_name)
 
-    def generate_images(self):
-        self.output_directory = tempfile.mkdtemp()
+    @staticmethod
+    def generate_images(stl_directory_name):
+        output_directory = tempfile.mkdtemp()
 
         args = ['blender',
                 '-b', '-P', 'image_match_generator.py', '--',
-                '-d', abspath(expanduser(self.stl_directory_name)),
-                '-o', self.output_directory,
+                '-d', abspath(expanduser(stl_directory_name)),
+                '-o', output_directory,
                 '--no-rotations',
                 '--only-front-view']
 
         spawnvp(P_WAIT, 'blender', args)
-        return self.output_directory
+        return output_directory
 
-    def search_images(self):
-        pass
+    def search_images(self, _images_directory):
+        img_paths = [join(_images_directory, x) for x in listdir(_images_directory) if splitext(x)[-1] == '.png']
+        for img_path in img_paths:
+            pass
 
-    def run(self):
-        path = self.generate_images()
-        #  self.search_images()
+    def run(self, stl_directory_name):
+        images_path = self.generate_images(stl_directory_name)
+        self.search_images(images_path)
