80f04f15c447ec4a20e71d502dbd9ee645a1b1cc
ryan <hendersonry@gmail.com>
2015-11-30 17:30:08 +0100

    add ascribe api

--- api_operations.py
+++ api_operations.py
@@ -1,51 +1,89 @@
 from three_d_match import ThreeDSearch
 from shutil import copy
-from elasticsearch.helpers import bulk
+from elasticsearch.helpers import bulk, BulkIndexError
 from os.path import join, abspath, expanduser
 from os import listdir, spawnvp, P_WAIT
 from image_match.signature_database import make_record
 import tempfile
 from shutil import rmtree
+from ascribe import AscribeWrapper
 
-#from generate_images_for_humans import ImagesBuilder
+# Ascribe credentials
+BEARER_TOKEN = '1b26f1ab053facda3bd4263c4d102d1404d3a008'
+
 
 class APIOperations(ThreeDSearch):
+    def __init__(self, es_nodes=['localhost'],
+                 index_name='***REMOVED***_tester',
+                 artist_name='***REMOVED***',
+                 cutoff=0.5):
+        super(APIOperations, self).__init__(es_nodes=es_nodes,
+                                            index_name=index_name,
+                                            cutoff=cutoff)
+
+        self.ascribe_wrapper = AscribeWrapper(BEARER_TOKEN)
+        self.artist_name = artist_name
 
     def add(self, stl_id, stl_file, origin=None, doc_type='image'):
         input_directory = tempfile.mkdtemp()
         output_directory = tempfile.mkdtemp()
-        copy(stl_file, input_directory)
+        try:
+            copy(stl_file, input_directory)
 
-        blender_args = ['blender',
-                        '-b', '-P', 'image_match_generator.py', '--',
-                        '-d', abspath(expanduser(input_directory)),
-                        '-o', output_directory,
-                        ]
-        self.generate_images(input_directory, blender_args=blender_args)
+            blender_args = ['blender',
+                            '-b', '-P', 'image_match_generator.py', '--',
+                            '-d', abspath(expanduser(input_directory)),
+                            '-o', output_directory,
+                            ]
+            self.generate_images(input_directory, blender_args=blender_args)
 
-        to_insert = []
+            to_insert = []
 
-        for image_path in listdir(output_directory):
-            if image_path.split('.')[-1] != 'csv':
-                rec = make_record(join(output_directory, image_path),
-                                  self.ses.gis,
-                                  self.ses.k,
-                                  self.ses.N)
+            # ascribe the file
+            piece = {
+                # file upload not implemented yet...coming soon?
+                'file': self._encrypt(stl_file),
+                'artist_name': self.artist_name,
+                'title': stl_id
+            }
 
-                rec['stl_id'] = stl_id
-                if origin:
-                    rec['origin'] = origin
+            ascribe_response = self.ascribe_wrapper(piece)
 
-                to_insert.append({
-                    '_index': self.ses.index,
-                    '_type': doc_type,
-                    '_source': rec
-                })
+            for image_path in listdir(output_directory):
+                if image_path.split('.')[-1] != 'csv':
+                    rec = make_record(join(output_directory, image_path),
+                                      self.ses.gis,
+                                      self.ses.k,
+                                      self.ses.N)
 
-        _, errs = bulk(self.es, to_insert)
-        rmtree(input_directory)
-        rmtree(output_directory)
+                    rec['ascribe_hash'] = ascribe_response['piece']['digital_work']['hash']
+                    rec['ascribe_id'] = ascribe_response['piece']['digital_work']['id']
+                    rec['ascribe_url'] = ascribe_response['piece']['digital_work']['url_safe']
 
+                    rec['stl_id'] = stl_id
+                    if origin:
+                        rec['origin'] = origin
+
+                    to_insert.append({
+                        '_index': self.ses.index,
+                        '_type': doc_type,
+                        '_source': rec
+                    })
+
+            _, errs = bulk(self.es, to_insert)
+
+        finally:
+            rmtree(input_directory)
+            rmtree(output_directory)
+
+        return ascribe_response
+
+    def _encrypt(self, stl_file):
+        return stl_file
+
+    def _decrypt(self, encrypted_stl_file):
+        return encrypted_stl_file
+    
     def search(self, stl_file=None, return_raw=False, ranking='single'):
         # TODO: include origin field
         input_directory = tempfile.mkdtemp()
@@ -59,17 +97,20 @@ class APIOperations(ThreeDSearch):
         elif ranking == 'single':
             return {stl_file: self.best_single_image(res)}
 
-    def render(self, stl_id, stl_file):
+    def render(self, stl_file=None, stl_id=None):
         input_directory = tempfile.mkdtemp()
         output_dir = tempfile.mkdtemp()
-        copy(stl_file, input_directory)
-        blender_args = ['blender',
-                '-b', '-P', 'generate_images_for_humans.py', '--',
-                '-d', input_directory,
-                '-o', output_dir,
-                '--custom-name', stl_id
-                ]
-        spawnvp(P_WAIT, 'blender', blender_args)
+        if stl_id:
+            pass
+        else:
+            copy(stl_file, input_directory)
+            blender_args = ['blender',
+                    '-b', '-P', 'generate_images_for_humans.py', '--',
+                    '-d', input_directory,
+                    '-o', output_dir,
+                    '--custom-name', stl_id
+                    ]
+            spawnvp(P_WAIT, 'blender', blender_args)
         rmtree(input_directory)
         return output_dir
 
